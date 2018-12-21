b1bd294cbe1aeede1c4e6fc4f906f2476926b867
Michael Altair <michael.altair@gogiigames.com>
2018-12-21 15:27:16 -0400

    updated ui, theres popups

--- Krampus/Assets/GUIController.cs
+++ Krampus/Assets/GUIController.cs
@@ -1,18 +1,81 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GUIController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Image[] hearts;
+    [SerializeField] Sprite heartFull, heartEmpty;
+    [SerializeField] Text soulsCount;
+    [SerializeField] int jumpCost, runCost;
+
+    private static GUIController _instance;
+
+    public static GUIController Instance
     {
-        
+        get { return _instance; }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Awake()
     {
-        
+        _instance = this;
+    }
+
+    public void Start() {
+        UpdateSoulsCount(0);
+
+        UpdateHeartDisplay(GameManager.Instance.Lives);
+    }
+
+    public void UpdateHeartDisplay(int life) {
+        foreach(Image heart in hearts) {
+            heart.sprite = heartEmpty;
+        }
+
+        switch(life) {
+            case 1:
+                hearts[0].sprite = heartFull;
+                hearts[1].sprite = heartEmpty;
+                hearts[2].sprite = heartEmpty;
+                return;
+            case 2:
+                hearts[0].sprite = heartFull;
+                hearts[1].sprite = heartFull;
+                hearts[2].sprite = heartEmpty;
+                return;
+            case 3:
+                hearts[0].sprite = heartFull;
+                hearts[1].sprite = heartFull;
+                hearts[2].sprite = heartFull;
+                return;
+            default:
+                hearts[0].sprite = heartEmpty;
+                hearts[1].sprite = heartEmpty;
+                hearts[2].sprite = heartEmpty;
+                return;
+        }
+    }
+
+    public void UpdateSoulsCount(int numSouls) {
+        string msg = "Souls Devoured: ";
+        msg += numSouls;
+        soulsCount.text = msg;
+    }
+
+
+
+    public void JumpPressed() {
+        if(GameManager.Instance.Souls > jumpCost) {
+            Debug.Log("Jump!");
+            GameManager.Instance.Souls = GameManager.Instance.Souls - jumpCost;
+        }
+    }
+
+    public void RunPressed() {
+        if(GameManager.Instance.Souls > runCost) {
+            Debug.Log("RUUNNNN!");
+            GameManager.Instance.Souls = GameManager.Instance.Souls - runCost;
+        }
     }
 }
---
+++ Krampus/Assets/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+
+    [SerializeField] GameObject winPopUp, losePopUp;
+    
+    int lives = 3;
+    int souls = 0;
+
+    public static GameManager Instance
+    {
+        get { return _instance; }
+    }
+
+    public void Awake()
+    {
+        _instance = this;
+    }
+
+    public int Lives {
+        get {
+            return lives;
+        }
+        set {
+            lives = value;
+            GUIController.Instance.UpdateHeartDisplay(value);
+            if(lives <= 0) {
+                EndGame(false);
+            }
+        }
+    }
+
+    public int Souls {
+        get {
+            return souls;
+        } 
+        set {
+            souls = value;
+            GUIController.Instance.UpdateSoulsCount(value);
+        }
+    }
+
+    public void EndGame (bool win) {
+        if(win) {
+            winPopUp.SetActive(true);
+        } else {
+            losePopUp.SetActive(true);
+        }
+    }
+}
